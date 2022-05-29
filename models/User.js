daa8c14a64ceba525864349c7c53232171aafc09
Rodrigo <rodvalenciaa@gmail.com>
2022-05-28 17:07:24 -0700

    User file

---
+++ models/User.js
@@ -0,0 +1,43 @@
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const UserSchema = new Schema (
+    {
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+            match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please enter a valid email address']
+        },
+        username: {
+            type: String, 
+            required: true, 
+            unique: true,
+            trim: true,
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thoughts',
+            },
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User',
+            },
+        ],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false
+    }
+);
+
+const User = model('User', UserSchema);
+
+module.exports = User;
